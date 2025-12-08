a1b403d729ebc24a934a2d1e3391e84d2aa75fa8
boobaGreen <devboobagreen@gmail.com>
2025-12-08 17:19:22 +0100

    fix(pwa): use manifestFilename to force app.webmanifest

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      filename: 'app.webmanifest',
+      manifestFilename: 'app.webmanifest',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
         name: 'FELIS: Apex Hunter',
